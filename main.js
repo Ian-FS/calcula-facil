d2ad67a1e78ec5fa4914f61e299eb331f56c1677
Ian_França <iiann_2009@hotmail.com>
2023-04-06 13:44:24 -0300

    atualizado estrutura e estilo da pagina

---
+++ main.js
@@ -0,0 +1,7 @@
+import "./style.scss";
+
+const metragemTotalCarcaca = document.getElementById("carcass-footage");
+const extrusaoPontoRef = document.getElementById("ref-extrusion");
+const carcacaPontoRef = document.getElementById("ref-carcass");
+const invalidoExtrusao = document.getElementById("invalid-extrusion");
+const carcacaSentido = document.getElementById("check");
