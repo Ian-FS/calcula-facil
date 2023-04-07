d468e84c64ba3e2f06215396471da2f2046d3fc0
Ian_França <iiann_2009@hotmail.com>
2023-04-07 16:04:43 -0300

    atualizado logica da pagina e estrutura

--- main.js
+++ main.js
@@ -5,3 +5,32 @@ const extrusaoPontoRef = document.getElementById("ref-extrusion");
 const carcacaPontoRef = document.getElementById("ref-carcass");
 const invalidoExtrusao = document.getElementById("invalid-extrusion");
 const carcacaSentido = document.getElementById("check");
+const botao = document.getElementById("button");
+
+const form = document.querySelector("form");
+
+console.log(metragemTotalCarcaca.innerHTML);
+
+botao.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (!carcacaSentido.checked) {
+    const valorCarcaca =
+      parseFloat(metragemTotalCarcaca.value) -
+      parseFloat(carcacaPontoRef.value) -
+      parseFloat(invalidoExtrusao.value);
+    const taxaCompressao =
+      100 - 100 / (valorCarcaca / parseFloat(extrusaoPontoRef.value));
+    console.log(taxaCompressao.toFixed(2) + "%");
+  } else {
+    metragemTotalCarcaca.style.display = "none";
+    invalidoExtrusao.style.display = "none";
+
+    const prodIdeal = parseFloat(carcacaPontoRef.value);
+    const prodReal = parseFloat(extrusaoPontoRef.value);
+
+    const taxaCompressao = 100 * (prodIdeal / prodReal);
+
+    console.log(taxaCompressao.toFixed(2) + "%");
+  }
+});
