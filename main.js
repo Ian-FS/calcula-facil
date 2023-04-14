e9c4f99964841222cbadcd067c6fb0d75fec0750
Ian_França <iiann_2009@hotmail.com>
2023-04-13 22:12:50 -0300

    atualizado logica, estilo e estrutura da página

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 import "./style.scss";
 
-const check = document.getElementById("check");
-const button = document.getElementById("button");
+const check = document.getElementById("check-box");
+const form = document.getElementById("form");
 
 check.onclick = removeInput;
-button.addEventListener("click", (e) => {
-  console.log(e.target.parentNode);
-  checkInput(e);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkInput();
 });
 
 function removeInput() {
@@ -22,46 +23,67 @@ function removeInput() {
 function calculaTaxa() {
   let taxaCompress = 0;
 
-  const totalCarcass = parseFloat(
-    document.getElementById("carcass-footage").value
-  );
-  const refExtrusion = parseFloat(
-    document.getElementById("ref-extrusion").value
-  );
+  const totalCarcass = parseFloat(document.getElementById("carcass-input").value);
+  const refExtrusion = parseFloat(document.getElementById("ref-extrusion-input").value);
 
-  const refCarcass = parseFloat(document.getElementById("ref-carcass").value);
+  const refCarcass = parseFloat(document.getElementById("ref-carcass-input").value);
 
-  const invalidExtrusion = parseFloat(
-    document.getElementById("invalid-extrusion").value
-  );
+  const invalidExtrusion = parseFloat(document.getElementById("invalid-extrusion-input").value);
 
   if (check.checked) {
     const validCarcass = refCarcass - invalidExtrusion;
     taxaCompress = 100 - 100 / (validCarcass / refExtrusion);
     console.log(
-      `Taxa de compressão de ${taxaCompress.toFixed(1)}%, equivalente a ${(
-        (validCarcass * taxaCompress) /
-        100
-      ).toFixed(1)} metros de tubo.`
+      `Taxa de compressão de ${taxaCompress.toFixed(1)}%, equivalente a ${((validCarcass * taxaCompress) / 100).toFixed(
+        1
+      )} metros de tubo.`
     );
   } else {
     const validCarcass = totalCarcass - refCarcass - invalidExtrusion;
     taxaCompress = 100 - 100 / (validCarcass / refExtrusion);
     console.log(
-      `Taxa de compressão de ${taxaCompress.toFixed(1)}%, equivalente a ${(
-        (validCarcass * taxaCompress) /
-        100
-      ).toFixed(2)} metros de tubo.`
+      `Taxa de compressão de ${taxaCompress.toFixed(1)}%, equivalente a ${((validCarcass * taxaCompress) / 100).toFixed(
+        2
+      )} metros de tubo.`
     );
   }
 }
-function checkInput(ev) {
-  ev.preventDefault();
 
-  if (check.checked) {
+function checkInput() {
+  const carcassInput = document.getElementById("carcass-input");
+  const refExtrusionInput = document.getElementById("ref-extrusion-input");
+  const refCarcassInput = document.getElementById("ref-carcass-input");
+  const invalidExtrusionInput = document.getElementById("invalid-extrusion-input");
+
+  const listInput = [carcassInput, refExtrusionInput, refCarcassInput, invalidExtrusionInput];
+
+  if (carcassInput.value === "" && !check.checked) {
+    setError(carcassInput, "Metragem total da Carcaça necessária");
+  }
+  if (refExtrusionInput.value === "") {
+    setError(refExtrusionInput, "Metragem de referência da camada de extrusão necessária");
+  }
+  if (refCarcassInput.value === "") {
+    setError(refCarcassInput, "Metragem de referência da carcaça necessária");
+  }
+  if (invalidExtrusionInput.value === "") {
+    setError(invalidExtrusionInput, "Metragem inválida da extrusão necessária");
+  } else {
+    calculaTaxa();
+    setSucess(listInput);
   }
 }
-
-function setError(e) {
-  console.log("faltou preencher");
+function setSucess(listInput) {
+  listInput.forEach((input) => {
+    input.value = "";
+    input.parentNode.className = "form-control";
+    input.parentNode.children[4].style.visibility = "hidden";
+    input.parentNode.children[4].style.position = "absolute";
+  });
+}
+function setError(input, message) {
+  input.parentNode.className = "form-control error";
+  input.parentNode.children[4].style.visibility = "visible";
+  input.parentNode.children[4].style.position = "relative";
+  input.parentNode.children[4].innerHTML = message;
 }
